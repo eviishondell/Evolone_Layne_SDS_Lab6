0686545baffa5b3841012111ae0fbeae70adb9c5
Evolone <evolonelayne@techexchange.in>
2022-03-22 04:39:42 +0000

    init commit≈

--- flask-template/app.py
+++ flask-template/app.py
@@ -1,22 +1,7 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# ---- YOUR APP STARTS HERE ----
-# -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import capital_check
 
 
 # -- Initialization section --
@@ -27,4 +12,28 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == 'GET':
+        return "Finish quiz to see answers"
+    else:
+        answers = { "new york": request.form['New York'], 
+                    "california": request.form['California'],
+                    "utah": request.form['Utah'],
+                    "alabama": request.form['Alabama'],
+                    "ohio": request.form['Ohio'],
+                    }
+        graded_answers = capital_check(answers)
+        new_york_answer = graded_answers['new york']
+        california_answer = graded_answers['california']
+        utah_answer = graded_answers['utah']
+        alabama_answer = graded_answers['alabama']
+        ohio_answer = graded_answers['ohio']
+        
+
+        return render_template('results.html', new_york_answer = new_york_answer,
+        california_answer = california_answer, utah_answer = utah_answer,
+        alabama_answer = alabama_answer, ohio_answer = ohio_answer)
